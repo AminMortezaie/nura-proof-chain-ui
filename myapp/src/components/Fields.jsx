4596adaf10a0e5ab73197a7551925610b5be0a71
aminmortezaie <mohammadmortezaie@gmail.com>
2022-09-06 18:15:17 +0430

    Fix: A little change in input css.

--- myapp/src/components/Fields.jsx
+++ myapp/src/components/Fields.jsx
@@ -6,8 +6,8 @@ const styles = {
     divRowStyle: "-mx-3 md:flex mb-6",
     divLabelInputStyle: "md:w-1/4 px-3",
     labelStyle: "uppercase tracking-wide text-black text-xs font-bold mb-2",
-    inputStyle: "w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3",
-    selectStyle: "block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500",
+    inputStyle: "w-full bg-gray-200 text-gray-700 border border-gray-200  rounded py-2.5 px-4 mb-3",
+    selectStyle: "w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 mb-3 rounded leading-tight hover:bg-white focus:outline-none focus:bg-white focus:border-gray-500",
     sectionButton: "md:mx-48 sm:mx-10 md:flex mt-2",
     divButton: "md:w-full px-3",
     buttonStyle: "flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
